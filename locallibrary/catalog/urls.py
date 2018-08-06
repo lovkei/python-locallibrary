6fea58941b32725545d1aef5357f86f1ace6afc7
lovkei <lin_kei@163.com>
2018-08-06 23:23:50 +0800

    modified:   catalog/admin.py 	modified:   catalog/models.py 	new file:   catalog/static/css/styles.css 	new file:   catalog/templates/base_generic.html 	new file:   catalog/templates/catalog/author_list.html 	new file:   catalog/templates/catalog/book_detail.html 	new file:   catalog/templates/catalog/book_list.html 	new file:   catalog/templates/index.html 	modified:   catalog/urls.py 	modified:   catalog/views.py

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,5 +1,12 @@
-from django.conf.urls import url
-from . import views
+from django.conf.urls import url 
+from django.urls import path
+from django.conf.urls import re_path
+from .import views
 
 urlpatterns = [
-    ]
+    path('', views.index, name='index'),
+    path('books/',views.BookListView.as_view(), name='books'),
+    path('authors/',views.AuthorListView.as_view(), name='authors'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,4 +1,48 @@
 from django.shortcuts import render
-from django.http import response
-from django.http import request
+from django.views import generic
 # Create your views here.
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    """
+    View function for home page of site.
+    """
+    # Generate counts of some of the main objects
+    num_books=Book.objects.all().count()
+    num_instances=BookInstance.objects.all().count()
+    # Available books (status = 'a')
+    num_instances_available=BookInstance.objects.filter(status__exact='o').count()
+    num_authors=Author.objects.count()  # The 'all()' is implied by default.
+
+    
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'index.html',
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+    )
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book    
+    def book_detail_view(request, pk):
+        try:
+            book_id=Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            raise Http404("Book does not exist")
+
+        return render(
+            request,
+            'catalog/book_detail.html',
+            context={'book':book_id,}
+        )
+
+    
